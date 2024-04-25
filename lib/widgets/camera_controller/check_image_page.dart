7a3daf12c48edba61f13cfecc245dd784907cb2d
andre <andreamonacelli02@gmail.com>
2024-04-25 19:56:47 +0200

    Initial Porting - Successfully styled check capture page

--- lib/widgets/camera_controller/check_image_page.dart
+++ lib/widgets/camera_controller/check_image_page.dart
@@ -4,6 +4,7 @@ import 'package:myidwallet_flutter/models/dbmanager/dbmanager.dart';
 import 'package:myidwallet_flutter/models/entities/document.dart';
 import 'package:myidwallet_flutter/routes.dart';
 import 'package:myidwallet_flutter/routes/doc_type_selection_page.dart';
+import 'package:myidwallet_flutter/widgets/header_text_card.dart';
 
 class CheckImagePage extends StatelessWidget{
 
@@ -29,69 +30,93 @@ class CheckImagePage extends StatelessWidget{
       },
     );
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Risultati scansione"),
-        centerTitle: true
-      ),
-      body: Column(
+      body: Stack(
         children: [
-          Container(
-            width: MediaQuery.of(context).size.width,
-            height: MediaQuery.of(context).size.height / _imageSectionHeightRatio,
-            color: Colors.white70,
-            alignment: Alignment.center,
-            child: Image.file(File(_imagePath))
+          Column(
+            children: [
+              HeaderTextCard("Risultati scansione", Colors.cyan, Colors.white70),
+              Expanded(
+                flex: 2,
+                child: Container(
+                    color: Colors.white70,
+                    child: Image.file(File(_imagePath))
+                )
+              ),
+              Flexible(
+                child: Container(
+                    margin: EdgeInsets.fromLTRB(5, 5, 5, 5),
+                    child: Form(
+                        key: _holderNameForm,
+                        child: textForm
+                    )
+                )
+              )
+            ]
           ),
-          Container(
-            margin: EdgeInsets.fromLTRB(5, 5, 5, 0),
-            child: Form(
-              key: _holderNameForm,
-              child: textForm
+          Padding(
+            padding: EdgeInsets.all(5),
+            child: Align(
+                alignment: Alignment.bottomLeft,
+                child: SizedBox(
+                  width: 200,
+                  height: 37,
+                  child: FloatingActionButton.extended(
+                      heroTag: "retry_fab",
+                      icon: Icon(Icons.arrow_back),
+                      extendedPadding: EdgeInsets.all(5),
+                      backgroundColor: Colors.grey,
+                      label: Text(
+                        "Scatta di nuovo la foto",
+                        style: TextStyle(
+                            backgroundColor: Colors.grey,
+                            fontSize: 15,
+                            color: Colors.black54
+                        ),
+                      ),
+                      onPressed: () {
+                        Navigator.of(context).pushNamed(RoutesManager.cameraScreenRoute);
+                      }
+                  ),
+                )
             )
           ),
-          Container(
-            margin: EdgeInsets.fromLTRB(5, 10, 5, 10),
-            child: FloatingActionButton.extended(
-              heroTag: "retry_fab",
-              backgroundColor: Colors.grey,
-              label: Text(
-                    "Scatta di nuovo la foto",
-                    style: TextStyle(
-                      backgroundColor: Colors.grey,
-                      fontSize: 12,
-                      color: Colors.black54
+          Padding(
+            padding: EdgeInsets.all(5),
+            child: Align(
+                alignment: Alignment.bottomRight,
+                child: SizedBox(
+                  width: 115,
+                  height: 37,
+                  child: FloatingActionButton.extended(
+                    icon: Icon(Icons.add, color: Colors.white70),
+                    extendedPadding: EdgeInsets.all(5),
+                    heroTag: "add_fab",
+                    backgroundColor: Colors.green,
+                    label: Text(
+                      "Aggiungi",
+                      style: TextStyle(
+                          backgroundColor: Colors.green,
+                          fontSize: 15,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white70
+                      ),
                     ),
-                ),
-              onPressed: () {
-                Navigator.of(context).pushNamed(RoutesManager.cameraScreenRoute);
-              }
+                    onPressed: () {
+                      ///TODO: Add the data processing snippet -> save it in the DB instantly
+                      //Document newDocument = Document(formController.text, DocTypeSelectionPage.selectedNation);
+                      //newDocument.documentTypeDescr = DocTypeSelectionPage.selectedType;
+                      Document newDocument = Document.forTesting(formController.text, DocTypeSelectionPage.selectedNation);
+                      newDocument.placeholderBGImage = DocTypeSelectionPage.placeholderBGImagePath;
+                      newDocument.generateDocumentGUID();
+                      DBManager.insertDocument(newDocument);
+                      Navigator.of(context).pushNamed(RoutesManager.homepageRoute);
+                    },
+                  )
+                )
             )
           )
         ],
-      ),
-      floatingActionButton: FloatingActionButton.extended(
-        heroTag: "add_fab",
-        backgroundColor: Colors.green,
-        label: Text(
-            "Aggiungi",
-            style: TextStyle(
-                backgroundColor: Colors.green,
-                fontSize: 15,
-                fontWeight: FontWeight.bold,
-                color: Colors.white70
-            ),
-        ),
-        onPressed: () {
-          ///TODO: Add the data processing snippet -> save it in the DB instantly
-          //Document newDocument = Document(formController.text, DocTypeSelectionPage.selectedNation);
-          //newDocument.documentTypeDescr = DocTypeSelectionPage.selectedType;
-          Document newDocument = Document.forTesting(formController.text, DocTypeSelectionPage.selectedNation);
-          newDocument.placeholderBGImage = DocTypeSelectionPage.placeholderBGImagePath;
-          newDocument.generateDocumentGUID();
-          DBManager.insertDocument(newDocument);
-          Navigator.of(context).pushNamed(RoutesManager.homepageRoute);
-        },
-      ),
+      )
     );
   }
 
